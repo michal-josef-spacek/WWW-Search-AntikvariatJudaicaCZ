ab813e42e468ebaabcbadb92042e949ef0cbdcb5
Michal Špaček <tupinek@gmail.com>
2014-09-05 23:36:31 +0200

    First real version.

--- AntikvariatJudaicaCZ.pm
+++ AntikvariatJudaicaCZ.pm
@@ -6,10 +6,8 @@ use strict;
 use warnings;
 
 # Modules.
-#use Encode qw(decode_utf8 encode_utf8);
 use LWP::UserAgent;
 use Readonly;
-#use Text::Iconv;
 use Web::Scraper;
 use WWW::Search qw(generic_option);
 
@@ -25,30 +23,22 @@ our $VERSION = 0.01;
 sub native_setup_search {
 	my ($self, $query) = @_;
 	$self->{'_def'} = scraper {
-#		process '//meta[@http-equiv="Content-Type"]', 'encoding' => [
-#			'@content',
-#			\&_get_encoding,
-#		];
-#		process '//table[@width="100"]/tr/td[5]/a',
-#			'next_url' => '@href';
-#		process '//td[@height="330"]/node()[3]', 'records' => 'RAW';
-#		process '//td[@height="330"]/table[@width="560"]', 'books[]'
-#			=> scraper {
-#
-#			process '//tr/td/font/a', 'title' => 'RAW',
-#				'url' => '@href';
-#			process '//tr/td[@width="136"]/font[2]',
-#				'price' => 'RAW';
-#			process '//tr[2]/td/font/strong', 'author' => 'RAW';
-#			process '//tr[3]/td/font/div', 'info' => 'RAW';
-#			process '//tr[4]/td/div/a', 'cover_url' => '@href';
-#			process '//tr[5]/td[1]/font[2]', 'publisher' => 'RAW';
-#			process '//tr[5]/td[2]/font[2]', 'year' => 'RAW';
-#			return;
-#		};
+		process '//div[@class="content"]/dl/div', 'books[]' => scraper {
+			process '//h2/a', 'title' => 'TEXT';
+			process '//h2/a', 'url' => '@href';
+			process '//img[@class="imagecache '.
+				'imagecache-product_list"]',
+				'cover_url' => '@src';
+			process '//div[@class="field sell-price"]',
+				'price' => 'TEXT';
+			process '//div[@class="field '.
+				'field-type-content-taxonomy '.
+				'field-field-author"]',
+				'author' => 'TEXT';
+			return;
+		};
 		return;
 	};
-	$self->{'_offset'} = 0;
 	$self->{'_query'} = $query;
 	return 1;
 }
@@ -61,12 +51,7 @@ sub native_retrieve_some {
 	my $ua = LWP::UserAgent->new(
 		'agent' => "WWW::Search::AntikvariatJudaicaCZ/$VERSION",
 	);
-	my $response = $ua->get($BASE_URL.$ACTION1
-#		'Content' => {
-#			'hltex' => $query,
-#			'hledani' => 'Hledat',
-#		},
-	);
+	my $response = $ua->get($BASE_URL.$ACTION1.$self->{'_query'});
 
 	# Process.
 	if ($response->is_success) {
@@ -77,16 +62,23 @@ sub native_retrieve_some {
 
 		# Process each book.
 		foreach my $book_hr (@{$books_hr->{'books'}}) {
-#			_fix_url($book_hr, 'cover_url');
-#			_fix_url($book_hr, 'url');
-#			push @{$self->{'cache'}}, $self->_process($book_hr);
+			_fix_url($book_hr, 'url');
+			$book_hr->{'price'} =~ s/\N{U+00A0}/ /ms;
+			$book_hr->{'price'} =~ s/^\s*Cena:\s*//ms;
+			$book_hr->{'author'} =~ s/^\s*Autor:\s*//ms;
+			push @{$self->{'cache'}}, $book_hr;
 		}
-
-		# Next url.
-#		_fix_url($books_hr, 'next_url');
-#		$self->next_url($books_hr->{'next_url'});
 	}
 
+	return;
+}
+
+# Fix URL to absolute path.
+sub _fix_url {
+	my ($book_hr, $url) = @_;
+	if (exists $book_hr->{$url}) {
+		$book_hr->{$url} = $BASE_URL.$book_hr->{$url};
+	}
 	return;
 }
 
